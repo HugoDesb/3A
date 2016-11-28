fc397ca4d8699277afdfc5d4d14bcb89fe64bb25
HugoDesb <hugo.desbiolles@gmail.com>
2016-11-28 13:48:49 +0100

    suppress .out files

--- Algo01/TD06/main.c
+++ Algo01/TD06/main.c
@@ -37,16 +37,21 @@ tpl ajout_liste(int val, tpl l);
 int tete_liste(tpl l);
 tpl queue_liste(tpl l);
 tpl creation_liste(tpl l);
+tpl creation_liste_ptr(tpl l);
 void afficher(tpl l);
 void afficher_liste(tpl l);
 int length(tpl l);
 int lengthWhile(tpl l);
+int length_ptr(tpl l);
+int lengthWhile_ptr(tpl l);
 tpl copie(tpl l);
 tpl searchFor(int x, tpl l);
+tpl copie_ptr(tpl l);
+tpl searchFor_ptr(int x, tpl l);
 tpl deleteFirst(int x, tpl l);
 tpl deleteAll(int x, tpl l);
 tpl copieInverse(tpl l);
-
+tpl copieInverse_ptr(tpl l);
 int main(void){
 
 	tpl l = cree_vide();
@@ -84,22 +89,26 @@ int main(void){
 	/* afficher(l); */
 	/* printf("Longueur de la liste (recur) : %d\n",length(l)); */
 	/* printf("Longueur de la liste (while) : %d\n",lengthWhile(l)); */
+	/* printf("Longueur de la liste (recur_ptr) : %d\n",length_ptr(l)); */
+	/* printf("Longueur de la liste (while_ptr) : %d\n",lengthWhile_ptr(l)); */
 
 	//Tests 5éme question
-	/* tpl newL; */
+	/* tpl newL,newL_ptr; */
 	/* l=creation_liste(l); */
 	/* newL = copie(l); */
+	/* newL_ptr = copie_ptr(l); */
 	/* printf("l : "); */
 	/* afficher(l); */
 	/* printf("newL : "); */
 	/* afficher(newL); */
-
-
+	/* printf("newL (ptr) : "); */
+	/* afficher(newL_ptr); */
 
 	//Tests 6éme question
 	/* l=creation_liste(l); */
 	/* int searchValue = 5; */
-	/* printf("Item found ? %d\n",!est_vide(searchFor(searchValue,l)));	 */
+	/* printf("Item found ? %d\n",!est_vide(searchFor(searchValue,l))); */
+	/* printf("Item found ? %d\n",!est_vide(searchFor_ptr(searchValue,l))); */
 
 	//Tests 7éme question a)
 	/* l=creation_liste(l); */
@@ -108,19 +117,34 @@ int main(void){
 	/* afficher(deleteFirst(searchValue, l)); */
 
 	//Tests 7éme question b)
-	l=creation_liste(l);
-	afficher(l);
-	int searchValue = 5;
-	afficher(deleteAll(searchValue, l));
+	/* l=creation_liste(l); */
+	/* afficher(l); */
+	/* int searchValue = 5; */
+	/* afficher(deleteAll(searchValue, l)); */
+
+	//Tests 8ème question
+	/* l=creation_liste(l); */
+	/* afficher(l); */
+	/* tpl newL = copieInverse(l); */
+	/* tpl newL_ptr = copieInverse_ptr(l); */
+	/* printf("Copie : "); */
+	/* afficher(newL); */
+	/* printf("Copie (ptr): "); */
+	/* afficher(newL); */
 	
 	return 0;
 }
-
+/**
+ * Fonction qui affiche l'ensemble de la liste
+ */
 void afficher(tpl l){
 	printf("(");
 	afficher_liste(l);
 	printf(")\n");
 }
+/**
+ * Fonction qui est utilisée par afficher(tpl l). NE PAS APPELER
+ */
 void afficher_liste(tpl l){
 	if(est_vide(queue_liste(l))){
 		printf("%d",tete_liste(l));
@@ -129,6 +153,11 @@ void afficher_liste(tpl l){
 		afficher_liste(queue_liste(l));
 	}
 }
+/**
+ * Fonction qui créé et rempli une liste selon les entrées
+ * de l'utilisateur.
+ * Un nombre négatif termine les entrées
+ */
 tpl creation_liste(tpl l){
 	int val;
 	do{
@@ -140,13 +169,43 @@ tpl creation_liste(tpl l){
 	}while(val>=0);
 	return l;
 }
+/**
+ * Fonction qui créé et rempli une liste selon les entrées
+ * de l'utilisateur.
+ * Un nombre négatif termine les entrées
+ * 
+ * Utilise les pointeurs
+ */
+tpl creation_liste_ptr(tpl l){
+	int val;
+	tpl newL;
+	do{
+		printf("Entrez une valeur : ");
+		scanf("%d",&val);
+			if(val>=0){
+				newL = (tpl)malloc(sizeof(t_list));
+				newL->val = val;
+				newL->next = l;
+				l = newL;
+		}
+	}while(val>=0);
+	return l;
+}
+/**
+ * Renvoie le pointeur vide
+ */
 tpl cree_vide(void){
 	return (tpl)NULL;
 }
-
+/**
+ * T si la liste est égale au pointeur null, F autrement
+ */
 int est_vide(tpl l){
 	return l==cree_vide();
 }
+/**
+ * Ajoute la valeur au début de la liste, renvoie la nouvelle liste
+ */
 tpl ajout_liste(int val, tpl l){
 	tpl newL;
 	newL = (tpl)malloc(sizeof(t_list));
@@ -154,14 +213,23 @@ tpl ajout_liste(int val, tpl l){
 	newL->next = l;
 	return newL;
 }
+/**
+ * Renvoie la première valeur de la liste
+ */
 int tete_liste(tpl l){
 	assert(l);
 	return l->val;
 }
+/**
+ * Renvoie le ptr vers l'élément suivant de la liste
+ */
 tpl queue_liste(tpl l){
 	assert(l);
 	return l->next;
 }
+/**
+ * Retourne la longueur de la liste
+ */
 int length(tpl l){
 	if(est_vide(l)){
 		return 0;
@@ -169,6 +237,20 @@ int length(tpl l){
 		return 1+length(queue_liste(l));
 	}
 }
+/**
+ * Retourne la longueur de la liste
+ * Utilise les pointeurs
+ */
+int length_ptr(tpl l){
+	if(l==(tpl)NULL){
+		return 0;
+	}else{
+		return 1+length(l->next);
+	}
+}
+/**
+ * Retourne la longueur de la liste
+ */
 int lengthWhile(tpl l){
 	int length = 0;
 	while(!est_vide(l)){
@@ -177,7 +259,21 @@ int lengthWhile(tpl l){
 	}
 	return length;
 }
-
+/**
+ * Retourne la longueur de la liste
+ * Utilise les pointeurs
+ */
+int lengthWhile_ptr(tpl l){
+	int length = 0;
+	while(l==(tpl)NULL){
+		length++;
+		l = l->next;
+	}
+	return length;
+}
+/**
+ * Copie une liste en respectant l'ordre
+ */
 tpl copie(tpl l){
 	if(est_vide(l)){
 		return cree_vide();
@@ -185,6 +281,30 @@ tpl copie(tpl l){
 		return ajout_liste(tete_liste(l), copie(queue_liste(l)));
 	}
 }
+/**
+ * Copie une liste en respectant l'ordre
+ * Utilise les pointeurs
+ */
+tpl copie_ptr(tpl l){
+	tpl queueListe;
+	tpl newL;
+	if(l==(tpl)NULL){
+		return (tpl)NULL;
+	}else{
+		//copie de la queue de la liste
+		queueListe = copie_ptr(l->next);
+		//Ajout du premier element a la copie du reste
+		newL = (tpl)malloc(sizeof(t_list));
+		newL->val = l->val;
+		newL->next = queueListe;
+		return newL;
+	}
+}
+/**
+ * Recherche et renvoie l'adresse de l'élément de la liste dont la
+ * valeur est égale a X
+ * Si aucun élément trouvé, renvoie le pointeur NULL
+ */
 tpl searchFor(int x, tpl l){
 	if(est_vide(l)){
 		return cree_vide();
@@ -196,6 +316,28 @@ tpl searchFor(int x, tpl l){
 		}
 	}
 }
+/**
+ * Recherche et renvoie l'adresse de l'élément de la liste dont la
+ * valeur est égale a X
+ * Si aucun élément trouvé, renvoie le pointeur NULL
+ * Utilise les pointeurs
+ */
+tpl searchFor_ptr(int x, tpl l){
+	if(l==(tpl)NULL){
+		return (tpl)NULL;
+	}else{
+		if(l->val==x){
+			return l;
+		}else{
+			return searchFor_ptr(x, l->next);
+		}
+	}
+}
+/**
+ * Recherche et supprime la 1ère occurrence de l'élément de la liste
+ * dont la valeur est égale a X
+ * Renvoie la liste moins cet élément
+ */
 tpl deleteFirst(int x , tpl l){
 	tpl curr = l;
 	tpl suiv = l->next;
@@ -220,6 +362,11 @@ tpl deleteFirst(int x , tpl l){
 	}
 	return ret;
 }
+/**
+ * Recherche et supprime toutes les occurrences de l'élément de la liste
+ * dont la valeur est égale a X
+ * Renvoie la liste moins ces éléments
+ */
 tpl deleteAll(int x , tpl l){
 	tpl curr = l;
 	tpl suiv = l->next;
@@ -243,12 +390,36 @@ tpl deleteAll(int x , tpl l){
 	}
 	return ret;
 }
+/**
+ * Effectue une copie de la liste donnée en inversant l'ordre des valeurs
+ * de cette dernière
+ */
 tpl copieInverse(tpl l){
 	tpl copie = cree_vide();
 	tpl curr = l;
 	while(!est_vide(curr)){
 		copie = ajout_liste(tete_liste(curr), copie);
-		curr = tete_liste(curr);
+		curr = queue_liste(curr);
+	}
+	return copie;
+}
+/**
+ * Effectue une copie de la liste donnée en inversant l'ordre des valeurs
+ * de cette dernière
+ * Utilise les pointeurs
+ */
+tpl copieInverse_ptr(tpl l){
+	tpl copie = (tpl)NULL;
+	tpl curr = l;
+	tpl newL;
+	while(curr!=(tpl)NULL){
+		//Ajout de l'élément dans copie
+		newL = (tpl)malloc(sizeof(t_list));
+		newL->val = l->val;
+		newL->next = copie;
+		copie = newL;
+
+		curr = curr->next;
 	}
 	return copie;
 }
