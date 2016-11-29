a657e88cee317bb746addada905583cbb4da6a83
HugoDesb <hugo.desbiolles@gmail.com>
2016-11-29 16:03:21 +0100

    -->6 TD07 Algo

--- Algo01/TD07-LesArbres/main.c
+++ Algo01/TD07-LesArbres/main.c
@@ -22,404 +22,177 @@
 #include <assert.h>
 #include <malloc.h>
 
-#define N1 5
 
-
-typedef struct list{
+typedef struct tree{
+	int type;
 	int val;
-	struct list *  next;
-} t_list, * tpl;
+	struct tree * left;
+	struct tree * right;
+	char tag;
+} t_tree, * tpa;
 
+int main(void);
+tpa cree_feuille(int val);
+int val_feuille(tpa a);
+int est_feuille(tpa a);
+tpa cree_noeud(char tag,tpa left,tpa right);
+tpa arbre_fg(tpa a);
+tpa arbre_fd(tpa a);
+char arbre_etiq(tpa a);
+void afficher(tpa a);
+int nb_nodes(tpa a);
+int nb_sheets(tpa a);
+int depth(tpa a);
+int sum(tpa a);
+tpa mirror(tpa a);
 
-tpl cree_vide(void);
-int est_vide(tpl l);
-tpl ajout_liste(int val, tpl l);
-int tete_liste(tpl l);
-tpl queue_liste(tpl l);
-tpl creation_liste(tpl l);
-tpl creation_liste_ptr(tpl l);
-void afficher(tpl l);
-void afficher_liste(tpl l);
-int length(tpl l);
-int lengthWhile(tpl l);
-int length_ptr(tpl l);
-int lengthWhile_ptr(tpl l);
-tpl copie(tpl l);
-tpl searchFor(int x, tpl l);
-tpl copie_ptr(tpl l);
-tpl searchFor_ptr(int x, tpl l);
-tpl deleteFirst(int x, tpl l);
-tpl deleteAll(int x, tpl l);
-tpl copieInverse(tpl l);
-tpl copieInverse_ptr(tpl l);
 int main(void){
+	/*Question 2 - Trois arbres*/
 
-	tpl l = cree_vide();
+	//values
+	tpa a = cree_feuille(1);
+	tpa b = cree_feuille(2);
+	tpa c = cree_feuille(3);
+	tpa d = cree_feuille(4);
+	tpa e = cree_feuille(5);
+	tpa f = cree_feuille(6);
+	tpa g = cree_feuille(7);
+	tpa h = cree_feuille(9);
+	tpa i = cree_feuille(16);
 
-	//Tests première question
-	/* int shouldBeEmpty = est_vide(l); */
-	/* printf("shouldBeEmpty : %d\n",shouldBeEmpty); */
-	/* l = ajout_liste(4,l); */
-
-	/* int headShouldBeFour = (4==tete_liste(l)); */
-	/* printf("headShouldBeFour : %d\n",headShouldBeFour); */
-	/* int queueShouldBeEmpty = est_vide(queue_liste(l)); */
-	/* printf("queueshouldbeempty : %d\n",queueShouldBeEmpty); */
-	/* l = ajout_liste(5, l); */
-
-	/* int headShouldBeFive = (5==tete_liste(l)); */
-	/* printf("headshouldbefive : %d\n", headShouldBeFive); */
-	/* int queueShouldBeFour = (4==tete_liste(queue_liste(l))); */
-	/* printf("queueshouldbefour : %d\n", queueShouldBeFour); */
-
-	//Tests 2ème question
-	/* l = creation_liste(l); */
-	/* afficher(l); */
+	//num 1
+	tpa fadd = cree_noeud('+', a, b);
+	tpa fmul1 = cree_noeud('*', e, f);
+	tpa fmul2 = cree_noeud('*', fadd, c);
+	tpa fmul3 = cree_noeud('*', d, fmul1);
+	tpa firstTree = cree_noeud('+', fmul2, fmul3);
+	afficher(firstTree);printf("\n");
+	printf("nbNodes : %d\n",nb_nodes(firstTree));
+	printf("nbSheets : %d\n",nb_sheets(firstTree));
+	printf("depth : %d\n",depth(firstTree));
+	printf("sum : %d\n",sum(firstTree));
+	printf("mirror :");
+	afficher(mirror(firstTree));printf("\n");
 	
-	//Tests 3ème question
-	/* l = ajout_liste(5, l); */
-	/* l = ajout_liste(4, l); */
-	/* l = ajout_liste(3, l); */
-	/* l = ajout_liste(2, l); */
-	/* l = ajout_liste(1, l); */
-	/* afficher(l); */
-
-	//Tests 4éme question
-	/* l = creation_liste(l); */
-	/* afficher(l); */
-	/* printf("Longueur de la liste (recur) : %d\n",length(l)); */
-	/* printf("Longueur de la liste (while) : %d\n",lengthWhile(l)); */
-	/* printf("Longueur de la liste (recur_ptr) : %d\n",length_ptr(l)); */
-	/* printf("Longueur de la liste (while_ptr) : %d\n",lengthWhile_ptr(l)); */
-
-	//Tests 5éme question
-	/* tpl newL,newL_ptr; */
-	/* l=creation_liste(l); */
-	/* newL = copie(l); */
-	/* newL_ptr = copie_ptr(l); */
-	/* printf("l : "); */
-	/* afficher(l); */
-	/* printf("newL : "); */
-	/* afficher(newL); */
-	/* printf("newL (ptr) : "); */
-	/* afficher(newL_ptr); */
-
-	//Tests 6éme question
-	/* l=creation_liste(l); */
-	/* int searchValue = 5; */
-	/* printf("Item found ? %d\n",!est_vide(searchFor(searchValue,l))); */
-	/* printf("Item found ? %d\n",!est_vide(searchFor_ptr(searchValue,l))); */
-
-	//Tests 7éme question a)
-	/* l=creation_liste(l); */
-	/* afficher(l); */
-	/* int searchValue = 5; */
-	/* afficher(deleteFirst(searchValue, l)); */
-
-	//Tests 7éme question b)
-	/* l=creation_liste(l); */
-	/* afficher(l); */
-	/* int searchValue = 5; */
-	/* afficher(deleteAll(searchValue, l)); */
-
-	//Tests 8ème question
-	/* l=creation_liste(l); */
-	/* afficher(l); */
-	/* tpl newL = copieInverse(l); */
-	/* tpl newL_ptr = copieInverse_ptr(l); */
-	/* printf("Copie : "); */
-	/* afficher(newL); */
-	/* printf("Copie (ptr): "); */
-	/* afficher(newL); */
+	//num 2
+	tpa sadd1 = cree_noeud('+', a, b);
+	tpa sadd2 = cree_noeud('+', sadd1, c);
+	tpa sadd3 = cree_noeud('+', sadd2, d);
+	tpa smul = cree_noeud('*', e, f);
+	tpa sadd4 = cree_noeud('+', smul, g);
+	tpa secondTree = cree_noeud('+', sadd3, sadd4);
+	afficher(secondTree);printf("\n");
+	printf("nbNodes : %d\n",nb_nodes(secondTree));
+	printf("nbSheets : %d\n",nb_sheets(secondTree));
+	printf("depth : %d\n",depth(secondTree));
+	printf("sum : %d\n",sum(secondTree));
+		printf("mirror :");
+	afficher(mirror(secondTree));printf("\n");
+	
+	//num 3
+	tpa tadd1 = cree_noeud('+', f, h);
+	tpa tmul1 = cree_noeud('*', e, tadd1);
+	tpa tmul2 = cree_noeud('*', i, b);
+	tpa tdiv1 = cree_noeud('/', d, b);
+	tpa tdiv2 = cree_noeud('/', tmul2, tdiv1);
+	tpa thirdTree = cree_noeud('-', tmul1, tdiv2);
+	afficher(thirdTree);printf("\n");
+	printf("nbNodes : %d\n",nb_nodes(thirdTree));
+	printf("nbSheets : %d\n",nb_sheets(thirdTree));
+	printf("depth : %d\n",depth(thirdTree));
+	printf("sum : %d\n",sum(thirdTree));
+	printf("mirror :");
+	afficher(mirror(thirdTree));printf("\n");
 	
 	return 0;
 }
-/**
- * Fonction qui affiche l'ensemble de la liste
- */
-void afficher(tpl l){
-	printf("(");
-	afficher_liste(l);
-	printf(")\n");
-}
-/**
- * Fonction qui est utilisée par afficher(tpl l). NE PAS APPELER
- */
-void afficher_liste(tpl l){
-	if(est_vide(queue_liste(l))){
-		printf("%d",tete_liste(l));
+
+void afficher(tpa a){
+	if(est_feuille(a)){
+		printf("%d",val_feuille(a));
 	}else{
-		printf("%d,",tete_liste(l));
-		afficher_liste(queue_liste(l));
+		printf("(");
+		afficher(arbre_fg(a));
+		printf(" %c ",arbre_etiq(a));
+		afficher(arbre_fd(a));
+		printf(")");
 	}
 }
-/**
- * Fonction qui créé et rempli une liste selon les entrées
- * de l'utilisateur.
- * Un nombre négatif termine les entrées
- */
-tpl creation_liste(tpl l){
-	int val;
-	do{
-		printf("Entrez une valeur : ");
-		scanf("%d",&val);
-		if(val>=0){
-			l = ajout_liste(val, l);
-		}
-	}while(val>=0);
-	return l;
+
+tpa cree_feuille(int val){
+	tpa sheet;
+	sheet = (tpa)malloc(sizeof(t_tree));
+	sheet->val = val;
+	sheet->type = 1;
+	return sheet;
 }
-/**
- * Fonction qui créé et rempli une liste selon les entrées
- * de l'utilisateur.
- * Un nombre négatif termine les entrées
- * 
- * Utilise les pointeurs
- */
-tpl creation_liste_ptr(tpl l){
-	int val;
-	tpl newL;
-	do{
-		printf("Entrez une valeur : ");
-		scanf("%d",&val);
-			if(val>=0){
-				newL = (tpl)malloc(sizeof(t_list));
-				newL->val = val;
-				newL->next = l;
-				l = newL;
-		}
-	}while(val>=0);
-	return l;
+
+
+int val_feuille(tpa a){
+	return a->val;
 }
-/**
- * Renvoie le pointeur vide
- */
-tpl cree_vide(void){
-	return (tpl)NULL;
+int est_feuille(tpa a){
+	return a->type;
 }
-/**
- * T si la liste est égale au pointeur null, F autrement
- */
-int est_vide(tpl l){
-	return l==cree_vide();
+tpa cree_noeud(char tag,tpa left,tpa right){
+	tpa node;
+	node = (tpa)malloc(sizeof(t_tree));
+	node->tag = tag;
+	node->left = left;
+	node->right = right;
+	node->type = 0;
+	return node;
 }
-/**
- * Ajoute la valeur au début de la liste, renvoie la nouvelle liste
- */
-tpl ajout_liste(int val, tpl l){
-	tpl newL;
-	newL = (tpl)malloc(sizeof(t_list));
-	newL->val = val;
-	newL->next = l;
-	return newL;
+tpa arbre_fg(tpa a){
+	return a->left;
 }
-/**
- * Renvoie la première valeur de la liste
- */
-int tete_liste(tpl l){
-	assert(l);
-	return l->val;
+
+tpa arbre_fd(tpa a){
+	return a->right;
 }
-/**
- * Renvoie le ptr vers l'élément suivant de la liste
- */
-tpl queue_liste(tpl l){
-	assert(l);
-	return l->next;
+char arbre_etiq(tpa a){
+	return a->tag;
 }
-/**
- * Retourne la longueur de la liste
- */
-int length(tpl l){
-	if(est_vide(l)){
+
+int nb_nodes(tpa a){
+	if(est_feuille(a)){
 		return 0;
 	}else{
-		return 1+length(queue_liste(l));
+		return 1+nb_nodes(arbre_fg(a))+nb_nodes(arbre_fd(a));
 	}
 }
-/**
- * Retourne la longueur de la liste
- * Utilise les pointeurs
- */
-int length_ptr(tpl l){
-	if(l==(tpl)NULL){
-		return 0;
+int nb_sheets(tpa a){
+	if(est_feuille(a)){
+		return 1;
 	}else{
-		return 1+length(l->next);
+		return nb_sheets(arbre_fg(a))+nb_sheets(arbre_fd(a));
 	}
 }
-/**
- * Retourne la longueur de la liste
- */
-int lengthWhile(tpl l){
-	int length = 0;
-	while(!est_vide(l)){
-		length++;
-		l = queue_liste(l);
-	}
-	return length;
-}
-/**
- * Retourne la longueur de la liste
- * Utilise les pointeurs
- */
-int lengthWhile_ptr(tpl l){
-	int length = 0;
-	while(l==(tpl)NULL){
-		length++;
-		l = l->next;
-	}
-	return length;
-}
-/**
- * Copie une liste en respectant l'ordre
- */
-tpl copie(tpl l){
-	if(est_vide(l)){
-		return cree_vide();
+int depth(tpa a){
+	int depthLeft, depthRight;
+	if(est_feuille(a)){
+		return 1;
 	}else{
-		return ajout_liste(tete_liste(l), copie(queue_liste(l)));
-	}
-}
-/**
- * Copie une liste en respectant l'ordre
- * Utilise les pointeurs
- */
-tpl copie_ptr(tpl l){
-	tpl queueListe;
-	tpl newL;
-	if(l==(tpl)NULL){
-		return (tpl)NULL;
-	}else{
-		//copie de la queue de la liste
-		queueListe = copie_ptr(l->next);
-		//Ajout du premier element a la copie du reste
-		newL = (tpl)malloc(sizeof(t_list));
-		newL->val = l->val;
-		newL->next = queueListe;
-		return newL;
-	}
-}
-/**
- * Recherche et renvoie l'adresse de l'élément de la liste dont la
- * valeur est égale a X
- * Si aucun élément trouvé, renvoie le pointeur NULL
- */
-tpl searchFor(int x, tpl l){
-	if(est_vide(l)){
-		return cree_vide();
-	}else{
-		if(tete_liste(l)==x){
-			return l;
-		}else{
-			return searchFor(x, queue_liste(l));
+		depthLeft = depth(arbre_fg(a));
+		depthRight = depth(arbre_fd(a));
+		if(depthLeft>=depthRight){
+			return 1+depthLeft;
+		}{
+			return 1+depthRight;
 		}
 	}
 }
-/**
- * Recherche et renvoie l'adresse de l'élément de la liste dont la
- * valeur est égale a X
- * Si aucun élément trouvé, renvoie le pointeur NULL
- * Utilise les pointeurs
- */
-tpl searchFor_ptr(int x, tpl l){
-	if(l==(tpl)NULL){
-		return (tpl)NULL;
+int sum(tpa a){
+	if(est_feuille(a)){
+		return val_feuille(a);
 	}else{
-		if(l->val==x){
-			return l;
-		}else{
-			return searchFor_ptr(x, l->next);
-		}
+		return sum(arbre_fg(a))+sum(arbre_fd(a));
 	}
 }
-/**
- * Recherche et supprime la 1ère occurrence de l'élément de la liste
- * dont la valeur est égale a X
- * Renvoie la liste moins cette éléments
- */
-tpl deleteFirst(int x , tpl l){
-	tpl curr = l;
-	tpl suiv = l->next;
-	tpl ret = l;
-	int found = 0;
-	
-	if(l->val != x){
-		while(suiv!=(tpl)NULL && !found){
-			if(suiv->val == x){
-				curr->next = suiv->next;
-				free(suiv);
-				found = 1;
-			}else{
-				curr = suiv;
-				suiv = suiv->next;
-			}
-		}
+tpa mirror(tpa a){
+	if(est_feuille(a)){
+		return a;
 	}else{
-		//Cas où l'élément a supprimer est le 1er
-		ret = l->next;
-		free(l);
+		return cree_noeud(arbre_etiq(a),mirror(arbre_fd(a)) , mirror(arbre_fg(a)));
 	}
-	return ret;
-}
-/**
- * Recherche et supprime toutes les occurrences de l'élément de la liste
- * dont la valeur est égale a X
- * Renvoie la liste moins ces éléments
- */
-tpl deleteAll(int x , tpl l){
-	tpl curr = l;
-	tpl suiv = l->next;
-	tpl ret = l;
-
-	
-	if(l->val != x){
-		while(suiv!=(tpl)NULL){
-			if(suiv->val == x){
-				curr->next = suiv->next;
-				free(suiv);
-			}else{
-				curr = suiv;
-				suiv = suiv->next;
-			}
-		}
-	}else{
-		//Cas où l'élément a supprimer est le 1er
-		ret = deleteAll(x,l->next);
-		free(l);
-	}
-	return ret;
-}
-/**
- * Effectue une copie de la liste donnée en inversant l'ordre des valeurs
- * de cette dernière
- */
-tpl copieInverse(tpl l){
-	tpl copie = cree_vide();
-	tpl curr = l;
-	while(!est_vide(curr)){
-		copie = ajout_liste(tete_liste(curr), copie);
-		curr = queue_liste(curr);
-	}
-	return copie;
-}
-/**
- * Effectue une copie de la liste donnée en inversant l'ordre des valeurs
- * de cette dernière
- * Utilise les pointeurs
- */
-tpl copieInverse_ptr(tpl l){
-	tpl copie = (tpl)NULL;
-	tpl curr = l;
-	tpl newL;
-	while(curr!=(tpl)NULL){
-		//Ajout de l'élément dans copie
-		newL = (tpl)malloc(sizeof(t_list));
-		newL->val = l->val;
-		newL->next = copie;
-		copie = newL;
-
-		curr = curr->next;
-	}
-	return copie;
 }
